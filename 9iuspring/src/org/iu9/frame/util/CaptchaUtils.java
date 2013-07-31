b25059276edcd24e5f61464f78cb073b8a729b32
屁屁果 <chunanyong@163.com>
2013-07-31 12:57:32 +0800

    提交 修改 验证码生成

--- 9iuspring/src/org/iu9/frame/util/CaptchaUtils.java
+++ 9iuspring/src/org/iu9/frame/util/CaptchaUtils.java
@@ -13,17 +13,18 @@ import javax.imageio.ImageIO;
 public class CaptchaUtils {
 
 	// 随机产生的字符串
-	private static final String RANDOM_STRS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String RANDOM_STRS = "23456789ABCDEFGHJKMNPQRSTUVWXYZ";
 
-	private static final String FONT_NAME = "Fixedsys";
-	private static final int FONT_SIZE = 18;
+	private static final String FONT_NAME = "Times New Roman";//字体
+	private static final int FONT_SIZE = 24;//字体大小
+	private int width = 85;// 图片宽
+	private int height = 25;// 图片高
+	private int lineNum = 155;// 干扰线数量
+	private int strNum = 4;// 随机产生字符数量
 
 	private Random random = new Random();
 
-	private int width = 80;// 图片宽
-	private int height = 25;// 图片高
-	private int lineNum = 50;// 干扰线数量
-	private int strNum = 4;// 随机产生字符数量
+
 
 	/**
 	 * 生成随机图片
@@ -31,23 +32,23 @@ public class CaptchaUtils {
 	public BufferedImage genRandomCodeImage(StringBuffer randomCode) {
 		// BufferedImage类是具有缓冲区的Image类
 		BufferedImage image = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_BGR);
+				BufferedImage.TYPE_INT_RGB);
 		// 获取Graphics对象,便于对图像进行各种绘制操作
 		Graphics g = image.getGraphics();
-		// 设置背景色
 		g.setColor(getRandColor(200, 250));
 		g.fillRect(0, 0, width, height);
-
-		// 设置干扰线的颜色
-		g.setColor(getRandColor(110, 120));
+		//g.setColor(new Color()); 
+		//g.drawRect(0,0,width-1,height-1); 
+		g.setColor(getRandColor(160, 200));
 
 		// 绘制干扰线
 		for (int i = 0; i <= lineNum; i++) {
 			drowLine(g);
 		}
 		// 绘制随机字符
-		g.setFont(new Font(FONT_NAME, Font.ROMAN_BASELINE, FONT_SIZE));
-		for (int i = 1; i <= strNum; i++) {
+		g.setFont(new Font(FONT_NAME, Font.PLAIN, FONT_SIZE));
+		
+		for (int i = 0; i < strNum; i++) {
 			randomCode.append(drowString(g, i));
 		}
 		g.dispose();
@@ -58,6 +59,7 @@ public class CaptchaUtils {
 	 * 给定范围获得随机颜色
 	 */
 	private Color getRandColor(int fc, int bc) {
+		Random random = new Random();
 		if (fc > 255)
 			fc = 255;
 		if (bc > 255)
@@ -72,12 +74,12 @@ public class CaptchaUtils {
 	 * 绘制字符串
 	 */
 	private String drowString(Graphics g, int i) {
-		g.setColor(new Color(random.nextInt(101), random.nextInt(111), random
-				.nextInt(121)));
+		g.setColor(new Color(20 + random.nextInt(110), 20 + random
+				.nextInt(110), 20 + random.nextInt(110)));
 		String rand = String.valueOf(getRandomString(random.nextInt(RANDOM_STRS
 				.length())));
-		g.translate(random.nextInt(3), random.nextInt(3));
-		g.drawString(rand, 13 * i, 16);
+	//	g.translate(random.nextInt(3), random.nextInt(3));
+		g.drawString(rand, 20 * i + 4, 19);
 		return rand;
 	}
 
@@ -87,8 +89,8 @@ public class CaptchaUtils {
 	private void drowLine(Graphics g) {
 		int x = random.nextInt(width);
 		int y = random.nextInt(height);
-		int x0 = random.nextInt(16);
-		int y0 = random.nextInt(16);
+		int x0 = random.nextInt(12);
+		int y0 = random.nextInt(12);
 		g.drawLine(x, y, x + x0, y + y0);
 	}
 
