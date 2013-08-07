72e1c4d1c086de4d14601cbb322a3fe5e7eb184d
屁屁果 <chunanyong@163.com>
2013-08-07 15:15:34 +0800

    修改 界面

--- springrain/src/org/springrain/frame/shiro/FramePermissionsAuthorizationFilter.java
+++ springrain/src/org/springrain/frame/shiro/FramePermissionsAuthorizationFilter.java
@@ -1,24 +1,38 @@
 package org.springrain.frame.shiro;
 
 import java.io.IOException;
+import java.util.Date;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authz.PermissionsAuthorizationFilter;
 import org.springframework.stereotype.Component;
+import org.springrain.demo.entity.Fwlog;
+import org.springrain.demo.service.IMenuService;
+import org.springrain.frame.util.DateUtils;
+import org.springrain.frame.util.IPUtils;
 @Component("frameperms")
 public class FramePermissionsAuthorizationFilter extends
 		PermissionsAuthorizationFilter {
+	public Logger logger=Logger.getLogger(getClass());
+	@Resource
+	private IMenuService menuService;
+	
 	@Override
 	public boolean isAccessAllowed(ServletRequest request,
 			ServletResponse response, Object mappedValue) throws IOException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		Subject subject = getSubject(request, response);
 		String uri = req.getRequestURI();
+		String requestURL = req.getRequestURL().toString();
 		String contextPath = req.getContextPath();
 		if(uri.endsWith("/pre")){// 去掉pre
 			uri=uri.substring(0,uri.length()-4);
@@ -30,7 +44,36 @@ public class FramePermissionsAuthorizationFilter extends
 		if(StringUtils.isBlank(uri)){
 			uri="/";
 		}
-		return subject.isPermitted(uri);
+		 boolean permitted = subject.isPermitted(uri);
+		 String isqx="否";
+		 if(permitted){
+			 isqx="是";
+		 }
+		 String ip = IPUtils.getClientAddress(req);
+		
+		 Fwlog fwlog=new Fwlog();
+		 fwlog.setFwUrl(requestURL);
+		 fwlog.setIsqx(isqx);
+		 fwlog.setIp(ip);
+			Subject user = SecurityUtils.getSubject();
+		 ShiroUser shiroUser = (ShiroUser) user.getPrincipals().getPrimaryPrincipal();
+		fwlog.setUserCode(shiroUser.getAccount());
+		fwlog.setUserName(shiroUser.getName());
+		Date startDate=new Date();
+		fwlog.setStartDate(startDate);
+		fwlog.setStrDate(DateUtils.convertDate2String("yyyy-MM-dd HH:mm:ss.SSSS", startDate));
+		HttpSession httpSession = req.getSession(false);
+		if(httpSession!=null){
+			fwlog.setSessionId(httpSession.getId());
+		}
+		try {
+			String menuName = menuService.getNameByPageurl(uri);
+			fwlog.setMenuName(menuName);
+			menuService.save(fwlog);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		return permitted;
 
 	}
 }
