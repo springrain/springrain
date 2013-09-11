d678ad01f24b54b5d353e23d52f37a1eabbb7779
ddatsh <admin@ddash.com>
2013-09-11 17:59:04 +0800

    JavaDoc Fix

--- springrain/src/org/springrain/frame/annotation/NotLog.java
+++ springrain/src/org/springrain/frame/annotation/NotLog.java
@@ -12,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
  * @copyright {@link 9iu.org}
  * @author springrain<9iuorg@gmail.com>
  * @version  2013-03-19 11:08:15
- * @see org.springrain.frame.annotation.PKSequence
+ * @see org.springrain.frame.annotation.NotLog
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotLog  {
