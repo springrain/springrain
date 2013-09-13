61b25a93af06261ffd5ba937de300b97f202c419
屁屁果 <chunanyong@163.com>
2013-09-13 09:43:51 +0800

    提交 复写 批量操作方法

--- springrain/src/org/springrain/demo/service/BaseDemoServiceImpl.java
+++ springrain/src/org/springrain/demo/service/BaseDemoServiceImpl.java
@@ -11,14 +11,15 @@ import org.springrain.frame.entity.IBaseEntity;
 import org.springrain.frame.service.BaseServiceImpl;
 import org.springrain.frame.util.Finder;
 
-
 @Service("baseDemoService")
-public class BaseDemoServiceImpl extends BaseServiceImpl implements IBaseDemoService {
-	
+public class BaseDemoServiceImpl extends BaseServiceImpl implements
+		IBaseDemoService {
+
 	@Resource
 	IBaseJdbcDao baseDemoDao;
-	
-	public BaseDemoServiceImpl(){}
+
+	public BaseDemoServiceImpl() {
+	}
 
 	@Override
 	public IBaseJdbcDao getBaseDao() {
@@ -27,74 +28,94 @@ public class BaseDemoServiceImpl extends BaseServiceImpl implements IBaseDemoSer
 
 	/**
 	 * 导入Excle文件
+	 * 
 	 * @param excel
 	 * @param clazz
 	 * @return
 	 * @throws Exception
 	 */
-	
-	public <T> String saveImportExcelFile(File excel,Class<T> clazz)throws Exception{
+	@Override
+	public <T> String saveImportExcelFile(File excel, Class<T> clazz)
+			throws Exception {
 		return super.saveImportExcelFile(excel, clazz);
 	}
-	
+
 	/**
 	 * Excel 导入时会循环调用该方法
+	 * 
 	 * @param entity
 	 * @return
 	 * @throws Exception
 	 */
-	
+
 	@Override
 	public String saveFromExcel(Object entity, int index, boolean istest,
 			List<String> listTitle) throws Exception {
-		if(istest){
+		if (istest) {
 			return null;
 		}
 		return save(entity).toString();
 	}
 
-   @Override
+	@Override
 	public Integer update(Finder finder) throws Exception {
 		return super.update(finder);
 	}
-   /**
-    * 保存一个对象
-    * @param <T>
-    * @param clazz
-    * @return
-    */
-	public  Object save( Object entity) throws Exception{
+
+	/**
+	 * 保存一个对象
+	 * 
+	 * @param <T>
+	 * @param clazz
+	 * @return
+	 */
+	@Override
+	public Object save(Object entity) throws Exception {
 		return super.save(entity);
 	}
-	 /**
-    * 更新一个对象
-    * @param <T>
-    * @param clazz
-    * @return
-    */
-	public  Integer update( IBaseEntity entity) throws Exception{
+
+	/**
+	 * 更新一个对象
+	 * 
+	 * @param <T>
+	 * @param clazz
+	 * @return
+	 */
+	@Override
+	public Integer update(IBaseEntity entity) throws Exception {
 		return super.update(entity);
 	}
 
 	/**
 	 * 根据Id 删除
+	 * 
 	 * @param id
 	 * @throws Exception
 	 */
-	public void deleteById(Object id,Class clazz) throws Exception{
-		 super.deleteById(id, clazz);
+	@Override
+	public void deleteById(Object id, Class clazz) throws Exception {
+		super.deleteById(id, clazz);
 	}
-	
+
 	/**
 	 * 判断主键是否有值 save or update 对象
+	 * 
 	 * @param entity
 	 * @return
 	 */
+	@Override
 	public Object saveorupdate(Object entity) throws Exception {
 		return super.saveorupdate(entity);
 	}
 
+	@Override
+	public List<Integer> update(List list) throws Exception {
+		return super.update(list);
+	}
 
-
+	@Override
+	public List<Integer> save(List list) throws Exception {
+		return super.save(list);
+	}
 
 }
