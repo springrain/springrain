571ebc14cca9da3cb0ee84d4ade90dd8a66750e5
屁屁果 <chunanyong@163.com>
2013-10-28 19:10:52 +0800

    强化 springUtils

--- springrain/src/org/springrain/frame/util/SpringUtils.java
+++ springrain/src/org/springrain/frame/util/SpringUtils.java
@@ -17,17 +17,14 @@ import org.springframework.stereotype.Component;
 public class SpringUtils  implements ApplicationContextAware {
 	
 	
-	private static ApplicationContext context;
+	private static ApplicationContext applicationContext;
 
-	/**
-	 * 
-	 */
 	public SpringUtils() {
 		
 	}
 	@Override
 	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.context=context;
+		this.applicationContext=context;
 	}
 	/**
 	 * 根据beanName 获取 spring bean
@@ -36,7 +33,23 @@ public class SpringUtils  implements ApplicationContextAware {
 	 */
 	public static  Object getBean(String beanName){
 		if(beanName==null)return null;
-	    return 	context.getBean(beanName);
+	    return 	applicationContext.getBean(beanName);
+	}
+	/**
+	 * 根据bean type 获取springBean
+	 * @param clazz
+	 * @return
+	 */
+	public static Object getBeanByType(Class clazz){
+		return applicationContext.getBean(clazz);
+	}
+	
+	/**
+	 * 获取 Spring applicationContext
+	 * @return
+	 */
+	public static ApplicationContext getContext() {
+		return applicationContext;
 	}
 	
 
