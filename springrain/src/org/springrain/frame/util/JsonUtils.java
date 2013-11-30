3830a265941c530f61a2fd8d0947bccb03b39e31
屁屁果 <chunanyong@163.com>
2013-11-30 09:59:08 +0800

    完善jsonUtils

--- springrain/src/org/springrain/frame/util/JsonUtils.java
+++ springrain/src/org/springrain/frame/util/JsonUtils.java
@@ -2,52 +2,68 @@ package org.springrain.frame.util;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.type.JavaType;
 
-public class JsonUtils {
-   static 	ObjectMapper mapper = null;
- 
-   static{
-	   mapper = new ObjectMapper();
-	   //为 null 的不转换
-	   mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL); 
-   }
- public static <T> T readValue(String content,Class<T> clazz) {
-	 T t=null;
-	try {
-		t=  mapper.readValue(content, clazz);
-	} catch (JsonParseException e) {
-		e.printStackTrace();
-	} catch (JsonMappingException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	return t;
- }
- 
- public static Object readValues(String content,Class CollectionType,Class clazz){
-	 Object o=null;
+public class JsonUtils  {
+	   static 	ObjectMapper mapper = null;
+	   public static Logger logger=Logger.getLogger(JsonUtils.class);
+	   static{
+		   mapper = new ObjectMapper();
+		   //为 null 的不转换
+		   mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL); 
+	   }
+	 public static <T> T readValue(String content,Class<T> clazz) {
+		 T t=null;
+		try {
+			t=  mapper.readValue(content, clazz);
+		} catch (JsonParseException e) {
+			logger.error(e);
+		} catch (JsonMappingException e) {
+			logger.error(e);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+		return t;
+	 }
 	 
-	try {
-		o=  mapper.readValue(content, getCollectionType(CollectionType, clazz));
-	} catch (JsonParseException e) {
-		e.printStackTrace();
-	} catch (JsonMappingException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	
-	return o;
- }
- 
- public static JavaType getCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {   
-         return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);   
-    }  
- 
+	 public static String writeValueAsString(Object o){
+		 String str=null;
+		try {
+			str= mapper.writeValueAsString(o);
+		} catch (JsonGenerationException e) {
+			logger.error(e);
+		} catch (JsonMappingException e) {
+			logger.error(e);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+		return str;
+	 }
+	 
+	 public static Object readValues(String content,Class CollectionType,Class clazz){
+		 Object o=null;
+		 
+		try {
+			o=  mapper.readValue(content, getCollectionType(CollectionType, clazz));
+		} catch (JsonParseException e) {
+			logger.error(e);
+		} catch (JsonMappingException e) {
+			logger.error(e);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+		
+		return o;
+	 }
+	 
+	 public static JavaType getCollectionType(Class<?> collectionClass, Class<?>... elementClasses) {   
+	         return mapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);   
+	    }  
+	 
 }
