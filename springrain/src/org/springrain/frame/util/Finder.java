fbaa3484f5b8bfc3ebb14718ae86a92f216c181e
屁屁果 <chunanyong@163.com>
2014-01-11 16:52:14 +0800

    添加 getUpdateFinder 并完善注释

--- springrain/src/org/springrain/frame/util/Finder.java
+++ springrain/src/org/springrain/frame/util/Finder.java
@@ -53,9 +53,24 @@ public class Finder {
 		params.put(param, value);
 		return this;
 	}
+	/**
+	 * 根据 Entity(分表)或者Class返回查询语句的Finder
+	 * 等同 getSelectFinder(User.class)==return new Finder("SELECT * FROM t_User ");
+	 * @param object
+	 * @return
+	 * @throws Exception
+	 */
 	public static Finder getSelectFinder(Object object) throws Exception{
 		return getSelectFinder(object, "*");
 	}
+	/**
+	 * 根据 Entity(分表)或者Class返回更新语句的Finder
+	 * 等同 getUpdateFinder(User.class,"id,name")==return new Finder("SELECT id,name FROM t_User ");
+	 * @param object,updatesql
+	 * @return
+	 * @throws Exception
+	 */
+	
 	public static Finder getSelectFinder(Object object,String selectsql) throws Exception{
 		if(object==null){
 			return null;
@@ -65,10 +80,51 @@ public class Finder {
 		}
 		String tableName=getTableName(object);
 		Finder finder=new Finder("SELECT ");
-		finder.append(selectsql).append(" FROM ").append(tableName);
+		finder.append(selectsql).append(" FROM ").append(tableName).append(" ");
 		return finder;
 		
 	}
+	
+	/**
+	 * 根据 Entity(分表)或者Class返回更新语句的Finder
+	 * 等同 getUpdateFinder(User.class)==return new Finder("UPDATE t_User SET ");
+	 * @param object
+	 * @return
+	 * @throws Exception
+	 */
+	public static Finder getUpdateFinder(Object object) throws Exception{
+		return getUpdateFinder(object, null);
+	}
+	
+	/**
+	 * 根据 Entity(分表)或者Class返回更新语句的Finder
+	 * 等同 getUpdateFinder(User.class,"name=:name")==return new Finder("UPDATE t_User SET name=:name ");
+	 * @param object,updatesql
+	 * @return
+	 * @throws Exception
+	 */
+	
+	public static Finder getUpdateFinder(Object object,String updatesql) throws Exception{
+		if(object==null){
+			return null;
+		}
+		String tableName=getTableName(object);
+		Finder finder=new Finder("UPDATE ");
+		finder.append(tableName).append(" SET ");
+		if(StringUtils.isNotBlank(updatesql)){
+			finder.append(updatesql).append(" ");
+		}
+		return finder;
+		
+	}
+	
+	/**
+	 * 根据 Entity(分表)或者Class返回删除语句的Finder
+	 * 等同 getDeleteFinder(User.class)==return new Finder("DELETE FROM  t_user ");
+	 * @param object
+	 * @return
+	 * @throws Exception
+	 */
 	
 	public static Finder getDeleteFinder(Object object) throws Exception{
 		if(object==null){
@@ -76,7 +132,7 @@ public class Finder {
 		}
 		String tableName=getTableName(object);
 		Finder finder=new Finder("DELETE FROM  ");
-		finder.append(tableName);
+		finder.append(tableName).append(" ");
 		return finder;
 	}
 	
