55e7b1043c92531d2e36121eb3d4dd4eec2da9b3
springrain <chunanyong@163.com>
2016-06-27 13:47:15 +0800

    处理特殊情况 安全管理器 为 null

--- springrain/src/main/java/org/springrain/frame/common/SessionUser.java
+++ springrain/src/main/java/org/springrain/frame/common/SessionUser.java
@@ -5,81 +5,90 @@ import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springrain.frame.shiro.ShiroUser;
 
-
-
 /**
  * 当前登录用户信息,可以在bean中调用获取当前登录用户信息,例如 SessionUser.getUserId()获取当前登录人的userId
+ * 
  * @copyright {@link 9iu.org}
  * @author springrain<9iuorg@gmail.com>
- * @version  2013-03-19 11:08:15
+ * @version 2013-03-19 11:08:15
  * @see org.springrain.frame.common.SessionUser
  */
 public class SessionUser {
-
 
 	public SessionUser() {
 	}
 
 	public static ShiroUser getShiroUser() {
-		Subject user = SecurityUtils.getSubject();
-		if(user==null){
-			return null;
-		}
-		ShiroUser shiroUser = (ShiroUser) user.getPrincipal();
-		if(shiroUser==null){
-			return null;
-		}
-		return shiroUser;
-	}
-	
-	public static Session getSession(){
-	 Subject user = SecurityUtils.getSubject();
-		if(user==null){
-			return null;
-		}
-		Session session = user.getSession();
-		return session;
-	} 
-	
 
+		try {
 
-	public static  String getUserId() {
-		ShiroUser shiroUser=getShiroUser();
-		if(shiroUser==null){
+			Subject user = SecurityUtils.getSubject();
+			if (user == null) {
+				return null;
+			}
+			ShiroUser shiroUser = (ShiroUser) user.getPrincipal();
+			if (shiroUser == null) {
+				return null;
+			}
+			return shiroUser;
+		} catch (Exception e) {
 			return null;
 		}
-		return shiroUser.getId();
 	}
 
-	public static  Integer getUserType() {
-		ShiroUser shiroUser=getShiroUser();
-		if(shiroUser==null){
+	public static Session getSession() {
+		try {
+			Subject user = SecurityUtils.getSubject();
+			if (user == null) {
+				return null;
+			}
+			Session session = user.getSession();
+			return session;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static String getUserId() {
+		try {
+			ShiroUser shiroUser = getShiroUser();
+			if (shiroUser == null) {
+				return null;
+			}
+			return shiroUser.getId();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static Integer getUserType() {
+		ShiroUser shiroUser = getShiroUser();
+		if (shiroUser == null) {
 			return null;
 		}
 		return shiroUser.getUserType();
 	}
 
-	
 	public static String getUserCode() {
-		ShiroUser shiroUser=getShiroUser();
-		if(shiroUser==null){
+		ShiroUser shiroUser = getShiroUser();
+		if (shiroUser == null) {
 			return null;
 		}
 		return shiroUser.getAccount();
-	
+
 	}
 
 	public static String getUserName() {
-		ShiroUser shiroUser=getShiroUser();
-		if(shiroUser==null){
+		ShiroUser shiroUser = getShiroUser();
+		if (shiroUser == null) {
 			return null;
 		}
 		return shiroUser.getName();
 	}
 
 	public static String getEmail() {
-		ShiroUser shiroUser=getShiroUser();
-		if(shiroUser==null){
+		ShiroUser shiroUser = getShiroUser();
+		if (shiroUser == null) {
 			return null;
 		}
 		return shiroUser.getEmail();
