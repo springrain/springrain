9a1aafe7671e3153226f08c1b55b2cc9c7d9714c
springrain <chunanyong@163.com>
2016-12-03 21:30:04 +0800

    修改 验证码 细节

--- springrain/src/main/java/org/springrain/frame/util/CaptchaUtils.java
+++ springrain/src/main/java/org/springrain/frame/util/CaptchaUtils.java
@@ -175,7 +175,7 @@ public class CaptchaUtils  {
 	        for (int i = 0; i < w1; i++) {
 	            double d = (double) (period >> 1)
 	                    * Math.sin((double) i / (double) period
-	                            + (4.2831853071795862D * (double) phase)
+	                            + (6.2831853071795862D * (double) phase)
 	                            / (double) frames);
 	            g.copyArea(i, 0, 1, h1, 0, (int) d);
 	            if (borderGap) {
