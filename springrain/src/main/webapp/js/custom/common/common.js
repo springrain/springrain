96414944d55a03df451abce5b020b27305d6f8be
AngeryFeather <2363882657@qq.com>
2016-12-28 17:50:21 +0800

    修改后台菜单

--- springrain/src/main/webapp/js/custom/common/common.js
+++ springrain/src/main/webapp/js/custom/common/common.js
@@ -1,89 +1,124 @@
 $(document).ready(function(){
-	var currentUrlHash = getLocationHashArr();
-	//加载菜单
-	ajaxmenu(currentUrlHash);
+    //加载菜单
+    if(!(!!locache.get("menuData"))){//没有数据
+    	ajaxmenu();
+    	var tmpData = locache.get("menuData");
+    	setCacheUrlPath("",tmpData);
+    	setCacheLeftMenu(tmpData);
+    }
+    var menuData = locache.get("menuData");
+    buildModule(menuData);
 });
+
+function setCacheUrlPath(pid,tmpData){	
+	for ( var index in tmpData) {
+		if(!(!!pid) || !(!!tmpData[index].pid)){
+			pid = tmpData[index].id;
+		}
+		var cacheVal = pid;
+		var cacheKey = ctx+tmpData[index]['pageurl']; 
+		var tmpLeaf = tmpData[index]['leaf'];
+		if(!!tmpLeaf){
+			setCacheUrlPath(pid,tmpLeaf);
+		}
+		if(!!cacheKey){
+			locache.set(cacheKey,cacheVal);
+		}
+	}
+}
+
+function setCacheLeftMenu(tmpData){
+	for ( var index in tmpData) {
+		locache.set(tmpData[index].id,tmpData[index]['leaf']);
+	}
+}
 
 /**
  * 获取所有导航资源
  */
-function ajaxmenu(currentUrlHash) {
-	jQuery.ajax({
-		url : ctx + "/system/menu/leftMenu",
-		type : "post",
-		cache : false,
-		async : false,
-		scriptCharset : "utf-8",
-		dataType : "json",
-		success : function(_json) {
-			if(_json.status=="success"){
-				buildModule(_json.data,currentUrlHash);
-			}
-		}
-	});
+function ajaxmenu() {
+    jQuery.ajax({
+        url : ctx + "/system/menu/leftMenu",
+        type : "post",
+        cache : false,
+        async : false,
+        scriptCharset : "utf-8",
+        dataType : "json",
+        success : function(_json) {
+            if(_json.status=="success"){
+            	locache.set("menuData",_json.data);
+            }
+        }
+    });
 }
 
-function buildModule(data,currentUrlHash) {
-	if (data != null && typeof (data) != "undefined") {
-		var htmlStr = '';
-		var firstPageHashVal = currentUrlHash[0];
-		var childrenMenuList = null;
-		for ( var i = 0; i < data.length; i++) {
-			var url = data[i].pageurl;
-			var hashVal = "#"+data[i].id;
-			var tmpData = data[i]['leaf'][0];
-			while(!!tmpData){
-				url = tmpData.pageurl;
-				hashVal += "#"+tmpData.id;
-				tmpData = tmpData['leaf'][0];
-			}
-			url = ctx + url + hashVal;
-			if((!!firstPageHashVal && data[i].id == firstPageHashVal) || (i==0)){//url中有第一个菜单的键值
-				htmlStr += '<li class="layui-nav-item layui-this"><a href="'+url+'">'+data[i].name+'</a></li>';
-				childrenMenuList = data[i]['leaf'];
-			}else{
-				htmlStr += '<li class="layui-nav-item"><a href="'+data[i]['leaf'][0].pageurl+'">'+data[i].name+'</a></li>';
-			}
-		}
-		$("#naviHeaderMenu").html(htmlStr);
-		$("#menu").html(getParentModule(childrenMenuList,currentUrlHash));
-	}
+function buildModule(data) {
+    if (data != null && typeof (data) != "undefined") {
+        var htmlStr = '';
+        var naviMenuId = locache.get(window.location.pathname);
+        var childrenMenuList = null;
+        for ( var i = 0; i < data.length; i++) {
+            var url = data[i].pageurl;
+            var tmpData = data[i]['leaf'][0];
+            while(!!tmpData){
+                url = tmpData.pageurl;
+                tmpData = tmpData['leaf'][0];
+            }
+            url = ctx + url;
+            if((data[i].id == naviMenuId) || (!(!!naviMenuId) && i==0)){//url中有第一个菜单的键值
+                htmlStr += '<li id="pmenu'+data[i].id+'" class="layui-nav-item layui-this"><a href="'+url+'">'+data[i].name+'</a></li>';
+                childrenMenuList = data[i]['leaf'];
+            }else{
+                htmlStr += '<li id="pmenu'+data[i].id+'" class="layui-nav-item"><a href="'+url+'">'+data[i].name+'</a></li>';
+            }
+        }
+        $("#naviHeaderMenu").html(htmlStr);
+        $("#menu").html(getParentModule(childrenMenuList));
+    }
 }
 
 
-function getParentModule(childrenMenuList,currentUrlHash) {
-	var htmlStr = '';
-	var secondMenuHashVal = currentUrlHash[1];
-	var showItem = "";
-	for(var i=0;i<childrenMenuList.length;i++){
-		if((!!secondMenuHashVal && childrenMenuList[i].id == secondMenuHashVal) || i==0){
-			showItem = "layui-nav-item";
-		}
-		htmlStr += '<li class="layui-nav-item '+showItem+'" id="'+childrenMenuList[i].id+'"><a href="';
-		var _leaf=childrenMenuList[i]["leaf"];
-		if(_leaf&&_leaf.length>0){
-			htmlStr = htmlStr+ ' javascript:;"> '+childrenMenuList[i].name+'</a>';
-			htmlStr = htmlStr+getChindModule(_leaf,currentUrlHash);
-		}else{
-			htmlStr = htmlStr+'javascript:;" >'+ childrenMenuList[i].name+'</a>';
-		}
-		htmlStr = htmlStr+'</li>';
-	}
-	return htmlStr;
+function getParentModule(childrenMenuList) {
+    var htmlStr = '';
+   
+    for(var i=0;i<childrenMenuList.length;i++){
+    	 var showItem = "";
+        if((ctx+childrenMenuList[i].pageurl) == window.location.pathname){
+            showItem = "layui-this";
+        }
+       
+        var _leaf=childrenMenuList[i]["leaf"];
+        if(_leaf&&_leaf.length>0){
+        	 htmlStr += '<li class="layui-nav-item '+showItem+'" id="'+childrenMenuList[i].id+'"><a href="';
+            htmlStr = htmlStr+ ' javascript:;"> '+childrenMenuList[i].name+'</a>';
+            htmlStr = htmlStr+getChindModule(_leaf);
+        }else{
+        	htmlStr += '<li class="layui-nav-item '+showItem+'" id="'+childrenMenuList[i].id+'"><a href="';
+        	 var url = childrenMenuList[i].pageurl;
+             var tmpData = childrenMenuList[i]['leaf'][0];
+             while(!!tmpData){
+                 url = tmpData.pageurl;
+                 tmpData = tmpData['leaf'][0];
+             }
+             url = ctx+url;
+            htmlStr = htmlStr+url+'" >'+ childrenMenuList[i].name+'</a>';
+        }
+        htmlStr = htmlStr+'</li>';
+    }
+    return htmlStr;
 }
 
-function getChindModule(_leaf,currentUrlHash) {
-	var thirdMenuHashVal = currentUrlHash[2];
-	var showItem = "";
-	var t = '<dl class="layui-nav-child">';
-	for ( var menuObj in _leaf) {
-		if((!!thirdMenuHashVal && _leaf[menuObj].id == thirdMenuHashVal) || menuObj==0){
-			showItem = "layui-this";
-		}
-		t = t+'<dd class="'+showItem+'" pageUrl="'+_leaf[menuObj].pageurl+'" id="'+_leaf[menuObj].id+'"><a href="javascript:;"><i class="layui-icon">&#xe611;</i><cite>'+_leaf[menuObj].name+'</cite></a></dd>';
-	}
-	t = t+'</dl>';
-	return t;
+function getChindModule(_leaf) {
+    var showItem = "";
+    var t = '<dl class="layui-nav-child">';
+    for ( var menuObj in _leaf) {
+        if((ctx+_leaf[menuObj].pageurl)==window.location.pathname){
+            showItem = "layui-this";
+        }
+        t = t+'<dd class="'+showItem+'" pageUrl="'+_leaf[menuObj].pageurl+'" id="'+_leaf[menuObj].id+'"><a href="javascript:;"><i class="layui-icon">&#xe611;</i><cite>'+_leaf[menuObj].name+'</cite></a></dd>';
+    }
+    t = t+'</dl>';
+    return t;
 }
 
 
@@ -92,13 +127,13 @@ function getChindModule(_leaf,currentUrlHash) {
  * @returns 数组
  */
 function getLocationHashArr(){
-	var urlHash = window.location.hash;
-	if(!!urlHash){
-		var urlHashArr = urlHash.split("#");
-		urlHashArr.splice(0,1);
-		return urlHashArr;
-	}
-	return new Array();
+    var urlHash = window.location.hash;
+    if(!!urlHash){
+        var urlHashArr = urlHash.split("#");
+        urlHashArr.splice(0,1);
+        return urlHashArr;
+    }
+    return new Array();
 }
 
 
---
+++ springrain/src/main/webapp/js/custom/common/message.js
@@ -0,0 +1,72 @@
+function myalert(message, fun) {
+	if (fun) {
+		// bootbox.alert(message,fun);
+		layer.alert(message, {
+			icon : 1
+		}, fun);
+	} else {
+		layer.alert(message, {
+			icon : 1
+		});
+	}
+}
+
+function myerror(message, fun) {
+	layer.alert(message, {
+		icon : 5
+	}, fun);
+}
+
+function myinfo(message, fun) {
+	layer.alert(message, {
+		icon : 0
+	}, fun);
+}
+
+function myquestion(message, fun) {
+	layer.alert(message, {
+		icon : 3
+	}, fun);
+}
+
+function mywarning(message, fun) {
+	layer.alert(message, {
+		icon : 2
+	}, fun);
+}
+
+function myconfirm(message, fun) {
+	/*
+	 * bootbox.confirm(message,function(result){ if(result&&fun){ fun(); }
+	 * 
+	 * });
+	 */
+
+	layer.confirm(message, {
+		btn : [ '确定', '取消' ]
+	// 按钮
+	}, function() {
+		fun();
+	}, function() {
+
+	});
+
+}
+
+function myprompt(message, fun) {
+
+	/*
+	 * bootbox.prompt(message,function(result){ if(fun){ fun(result); }
+	 * 
+	 * });
+	 */
+
+	layer.prompt({
+		title : message,
+		formType : 1
+	// prompt风格，支持0-2
+	}, function(pass) {
+		fun(pass);
+	});
+
+}
