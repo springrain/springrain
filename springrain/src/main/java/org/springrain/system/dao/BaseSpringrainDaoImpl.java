b83a742437a084ffe2e09e6834eaba51b23d26be
springrain <chunanyong@163.com>
2017-01-11 10:33:00 +0800

    增加 处理同步延迟方法,备用

--- springrain/src/main/java/org/springrain/system/dao/BaseSpringrainDaoImpl.java
+++ springrain/src/main/java/org/springrain/system/dao/BaseSpringrainDaoImpl.java
@@ -58,6 +58,19 @@ public class BaseSpringrainDaoImpl extends BaseJdbcDaoImpl implements IBaseJdbcD
 	public NamedParameterJdbcTemplate getJdbc() {
 		return jdbc;
 	}
+	
+	/*
+	 * 读写分离时,处理同步延迟问题
+	@Override
+	public NamedParameterJdbcTemplate getJdbc() {
+		try{
+            TransactionInterceptor.currentTransactionStatus();
+		}catch(NoTransactionException e){
+			return this.jdbc;
+		}
+		return getWriteJdbc();
+	}
+	*/
 
 
 /**
