304b67c3611fd2e2311e0476bcb91ae5bf9abe23
wu <wu@DESKTOP-P75ULAJ>
2017-01-19 14:32:24 +0800

    测试sql

--- springrain/sql/cms.sql
+++ springrain/sql/cms.sql
@@ -273,5 +273,3 @@ CREATE TABLE `cms_theme_template` (
 INSERT INTO `t_tableindex` (`id`, `maxIndex`, `prefix`) VALUES ('cms_site', '100', 's_');
 INSERT INTO `t_tableindex` (`id`, `maxIndex`, `prefix`) VALUES ('cms_channel', '1000', 'h_');
 INSERT INTO `t_tableindex` (`id`, `maxIndex`, `prefix`) VALUES ('cms_content', '10000', 'c_');
-
---test
