4f3f8a19b9cd0d5ca2afbc935189e888bb7f753f
springrain <chunanyong@163.com>
2017-02-06 12:42:48 +0800

    修改 site 拦截登录

--- springrain/src/main/java/org/springrain/frame/shiro/SiteUserFilter.java
+++ springrain/src/main/java/org/springrain/frame/shiro/SiteUserFilter.java
@@ -6,31 +6,33 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
 import org.springrain.frame.util.GlobalStatic;
+import org.springrain.frame.util.InputSafeUtils;
 
 /**
- * 系统后台管理用户的过滤器
+ * 站点管理人员的过滤器,验证token
  * @author caomei
  */
 
-@Component("systemuser")
-public class SystemUserFilter extends BaseUserFilter {
-	public SystemUserFilter(){
-		//跳转到登录界面
-	    super.setLoginUrl("/system/login");
-	}
+@Component("siteuser")
+public class SiteUserFilter extends BaseUserFilter {
 	
 	@Override
 	 protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
 		 boolean access=super.isAccessAllowed(request, response, mappedValue);
-		 if(!access){
+		 
+		 HttpServletRequest req=(HttpServletRequest) request;
+		 if(!access){//登录失败了,需要设置一下需要跳转的登录URL
+			 String siteId=InputSafeUtils.substringByURI(req.getRequestURI(), "/s_");
+			 request.setAttribute(GlobalStatic.customLoginURLKey, "/s/"+siteId+"/login");
 			 return access;
 		 } 
 		 
-		 HttpServletRequest req=(HttpServletRequest) request;
+		
 		 Object obj=req.getSession().getAttribute(GlobalStatic.tokenKey);
-		 if(obj==null||(!obj.toString().startsWith("system_"))){//tokenKey必须是system_开头
+		 
+		 if(obj==null||!(obj.toString().startsWith("s_")||obj.toString().startsWith("system_"))){//tokenKey必须是s_或者system_开头
 			 request.setAttribute(GlobalStatic.errorTokentoURLKey, GlobalStatic.errorTokentoURL);
-		    return false;
+		     return false;
 		 }
 	
 		String token=obj.toString();
--- springrain/src/main/java/org/springrain/frame/util/Enumerations.java
+++ springrain/src/main/java/org/springrain/frame/util/Enumerations.java
@@ -3,7 +3,7 @@ package org.springrain.frame.util;
 public class Enumerations {
 	
 	public enum OrgType{
-		部门(1),虚拟权限组(2),站长部门(10),微信订阅服务号(11),wap站(14),PC站(13),企业号(12),投票(15);
+		部门(1),虚拟权限组(2),站长部门(10),微信订阅服务号(11),企业号(12),PC站(13),wap站(14),投票(15);
 		int type;
 		private OrgType(Integer type){
 			this.type = type;
