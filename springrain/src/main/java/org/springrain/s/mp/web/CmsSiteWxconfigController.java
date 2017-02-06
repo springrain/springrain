7b7c5c426d8838c4a5c06379e5a68a7073e8246a
AngeryFeather <2363882657@qq.com>
2017-02-06 12:04:45 +0800

    添加微信配置信息管理

---
+++ springrain/src/main/java/org/springrain/s/mp/web/CmsSiteWxconfigController.java
@@ -0,0 +1,214 @@
+package  org.springrain.s.mp.web;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springrain.cms.entity.CmsSiteWxconfig;
+import org.springrain.frame.controller.BaseController;
+import org.springrain.frame.util.GlobalStatic;
+import org.springrain.frame.util.Page;
+import org.springrain.frame.util.ReturnDatas;
+import org.springrain.frame.util.property.MessageUtils;
+import org.springrain.s.mp.service.ICmsSiteWxconfigService;
+
+
+/**
+ * TODO 在此加入类描述
+ * @copyright {@link weicms.net}
+ * @author springrain<Auto generate>
+ * @version  2017-02-06 11:38:43
+ * @see org.springrain.cms.base.web.CmsSiteWxconfig
+ */
+@Controller
+@RequestMapping(value="/s/cms/wxconfig")
+public class CmsSiteWxconfigController  extends BaseController {
+	@Resource
+	private ICmsSiteWxconfigService cmsSiteWxconfigService;
+	
+	private String listurl="/s/mp/conf/confList";
+	
+	
+	   
+	/**
+	 * 列表数据,调用listjson方法,保证和app端数据统一
+	 * 
+	 * @param request
+	 * @param model
+	 * @param cmsSiteWxconfig
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/list")
+	public String list(HttpServletRequest request, Model model,CmsSiteWxconfig cmsSiteWxconfig) 
+			throws Exception {
+		ReturnDatas returnObject = listjson(request, model, cmsSiteWxconfig);
+		model.addAttribute(GlobalStatic.returnDatas, returnObject);
+		return listurl;
+	}
+	
+	/**
+	 * json数据,为APP提供数据
+	 * 
+	 * @param request
+	 * @param model
+	 * @param cmsSiteWxconfig
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/list/json")
+	public @ResponseBody
+	ReturnDatas listjson(HttpServletRequest request, Model model,CmsSiteWxconfig cmsSiteWxconfig) throws Exception{
+		ReturnDatas returnObject = ReturnDatas.getSuccessReturnDatas();
+		// ==构造分页请求
+		Page page = newPage(request);
+		// ==执行分页查询
+		List<CmsSiteWxconfig> datas=cmsSiteWxconfigService.findListDataByFinder(null,page,CmsSiteWxconfig.class,cmsSiteWxconfig);
+			returnObject.setQueryBean(cmsSiteWxconfig);
+		returnObject.setPage(page);
+		returnObject.setData(datas);
+		return returnObject;
+	}
+	
+	@RequestMapping("/list/export")
+	public void listexport(HttpServletRequest request,HttpServletResponse response, Model model,CmsSiteWxconfig cmsSiteWxconfig) throws Exception{
+		// ==构造分页请求
+		Page page = newPage(request);
+	
+		File file = cmsSiteWxconfigService.findDataExportExcel(null,listurl, page,CmsSiteWxconfig.class,cmsSiteWxconfig);
+		String fileName="cmsSiteWxconfig"+GlobalStatic.excelext;
+		downFile(response, file, fileName,true);
+		return;
+	}
+	
+		/**
+	 * 查看操作,调用APP端lookjson方法
+	 */
+	@RequestMapping(value = "/look")
+	public String look(Model model,HttpServletRequest request,HttpServletResponse response)  throws Exception {
+		ReturnDatas returnObject = lookjson(model, request, response);
+		model.addAttribute(GlobalStatic.returnDatas, returnObject);
+		return "/s/mp/conf/confLook";
+	}
+
+	
+	/**
+	 * 查看的Json格式数据,为APP端提供数据
+	 */
+	@RequestMapping(value = "/look/json")
+	public @ResponseBody
+	ReturnDatas lookjson(Model model,HttpServletRequest request,HttpServletResponse response) throws Exception {
+		ReturnDatas returnObject = ReturnDatas.getSuccessReturnDatas();
+		java.lang.String id=request.getParameter("id");
+		if(StringUtils.isNotBlank(id)){
+		  CmsSiteWxconfig cmsSiteWxconfig = cmsSiteWxconfigService.findCmsSiteWxconfigById(id);
+		   returnObject.setData(cmsSiteWxconfig);
+		}else{
+		returnObject.setStatus(ReturnDatas.ERROR);
+		}
+		return returnObject;
+		
+	}
+	
+	
+	/**
+	 * 新增/修改 操作吗,返回json格式数据
+	 * 
+	 */
+	@RequestMapping("/update")
+	public @ResponseBody
+	ReturnDatas saveorupdate(Model model,CmsSiteWxconfig cmsSiteWxconfig,HttpServletRequest request,HttpServletResponse response) throws Exception{
+		ReturnDatas returnObject = ReturnDatas.getSuccessReturnDatas();
+		returnObject.setMessage(MessageUtils.UPDATE_SUCCESS);
+		try {
+		
+			java.lang.String id =cmsSiteWxconfig.getId();
+			if(StringUtils.isBlank(id)){
+			  cmsSiteWxconfig.setId(null);
+			}
+		
+			cmsSiteWxconfigService.saveorupdate(cmsSiteWxconfig);
+			
+		} catch (Exception e) {
+			String errorMessage = e.getLocalizedMessage();
+			logger.error(errorMessage);
+			returnObject.setStatus(ReturnDatas.ERROR);
+			returnObject.setMessage(MessageUtils.UPDATE_ERROR);
+		}
+		return returnObject;
+	
+	}
+	
+	/**
+	 * 进入修改页面,APP端可以调用 lookjson 获取json格式数据
+	 */
+	@RequestMapping(value = "/update/pre")
+	public String updatepre(Model model,HttpServletRequest request,HttpServletResponse response)  throws Exception{
+		ReturnDatas returnObject = lookjson(model, request, response);
+		model.addAttribute(GlobalStatic.returnDatas, returnObject);
+		return "/s/mp/conf/confCru";
+	}
+	
+	/**
+	 * 删除操作
+	 */
+	@RequestMapping(value="/delete")
+	public @ResponseBody ReturnDatas delete(HttpServletRequest request) throws Exception {
+
+			// 执行删除
+		try {
+		java.lang.String id=request.getParameter("id");
+		if(StringUtils.isNotBlank(id)){
+				cmsSiteWxconfigService.deleteById(id,CmsSiteWxconfig.class);
+				return new ReturnDatas(ReturnDatas.SUCCESS,
+						MessageUtils.DELETE_SUCCESS);
+			} else {
+				return new ReturnDatas(ReturnDatas.WARNING,
+						MessageUtils.DELETE_WARNING);
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return new ReturnDatas(ReturnDatas.WARNING, MessageUtils.DELETE_WARNING);
+	}
+	
+	/**
+	 * 删除多条记录
+	 * 
+	 */
+	@RequestMapping("/delete/more")
+	public @ResponseBody
+	ReturnDatas deleteMore(HttpServletRequest request, Model model) {
+		String records = request.getParameter("records");
+		if(StringUtils.isBlank(records)){
+			 return new ReturnDatas(ReturnDatas.ERROR,
+					MessageUtils.DELETE_ALL_FAIL);
+		}
+		String[] rs = records.split(",");
+		if (rs == null || rs.length < 1) {
+			return new ReturnDatas(ReturnDatas.ERROR,
+					MessageUtils.DELETE_NULL_FAIL);
+		}
+		try {
+			List<String> ids = Arrays.asList(rs);
+			cmsSiteWxconfigService.deleteByIds(ids,CmsSiteWxconfig.class);
+		} catch (Exception e) {
+			return new ReturnDatas(ReturnDatas.ERROR,
+					MessageUtils.DELETE_ALL_FAIL);
+		}
+		return new ReturnDatas(ReturnDatas.SUCCESS,
+				MessageUtils.DELETE_ALL_SUCCESS);
+		
+		
+	}
+
+}
