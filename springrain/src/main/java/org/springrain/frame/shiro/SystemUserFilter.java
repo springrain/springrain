1add08ad5e1713d506fed8dca85e9f074b9f9ad4
springrain <chunanyong@163.com>
2017-02-06 14:59:56 +0800

    提交 修改 拦截 system,自定义登录界面

--- springrain/src/main/java/org/springrain/frame/shiro/SystemUserFilter.java
+++ springrain/src/main/java/org/springrain/frame/shiro/SystemUserFilter.java
@@ -4,8 +4,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springrain.frame.util.GlobalStatic;
+import org.springrain.frame.util.InputSafeUtils;
 
 /**
  * 系统后台管理用户的过滤器
@@ -22,11 +24,18 @@ public class SystemUserFilter extends BaseUserFilter {
 	@Override
 	 protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
 		 boolean access=super.isAccessAllowed(request, response, mappedValue);
-		 if(!access){
+		 
+		 HttpServletRequest req=(HttpServletRequest) request;
+		 if(!access){//登录失败了,需要设置一下需要跳转的登录URL
+			 String siteId=InputSafeUtils.substringByURI(req.getRequestURI(), "/s_");
+			 if(StringUtils.isNotBlank(siteId)){
+				 request.setAttribute(GlobalStatic.customLoginURLKey, "/system/"+siteId+"/login");
+			 }
+			
 			 return access;
 		 } 
 		 
-		 HttpServletRequest req=(HttpServletRequest) request;
+		
 		 Object obj=req.getSession().getAttribute(GlobalStatic.tokenKey);
 		 if(obj==null||(!obj.toString().startsWith("system_"))){//tokenKey必须是system_开头
 			 request.setAttribute(GlobalStatic.errorTokentoURLKey, GlobalStatic.errorTokentoURL);
