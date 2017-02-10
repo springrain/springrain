2576fa11a6c955330c238d5379835b6e9540dae5
hyy <hyy@DESKTOP-5UJ3BR8>
2017-02-10 10:45:42 +0800

    修改bug

--- springrain/src/main/webapp/js/custom/common/springrain.js
+++ springrain/src/main/webapp/js/custom/common/springrain.js
@@ -290,7 +290,7 @@
 				_time=_time?_time:2000;
 				
 				layer.msg(_msg, {
-					  icon: 1,
+					  icon: _icon,
 					  time: 2000 //2秒关闭（如果不配置，默认是3秒）
 					}, function(){
 						if (_fun&&typeof _fun =="function") {
