4091e102091f1b7471c136626a57ffde04cf03dd
springrain <chunanyong@163.com>
2017-02-21 11:44:01 +0800

    提交 修改微信 sdk config,基于接口实现

---
+++ springrain/src/main/java/org/springrain/weixin/sdk/common/api/IWxCpConfig.java
@@ -0,0 +1,15 @@
+package org.springrain.weixin.sdk.common.api;
+
+public interface IWxCpConfig extends IWxConfig {
+
+	String getCorpId();
+
+	String getOauth2redirectUri();
+
+	Integer getAgentId();
+
+	String getCorpSecret();
+
+	
+
+}
