1ca237b8e457bf4d28deb0af698ac8f2cfd1de57
caomei <caomei@DESKTOP-1T7L05U.lan>
2017-06-09 22:21:20 +0800

    修改测试用例

--- springrain/src/test/java/test/LuceneTest.java
+++ springrain/src/test/java/test/LuceneTest.java
@@ -37,12 +37,12 @@ public class LuceneTest {
 		
 		List<LuceneDto> list=new ArrayList<>();
 		
-		for (int i = 0; i < 500000; i++) {
+		for (int i = 0; i < 50; i++) {
 		    LuceneDto u=new LuceneDto();
 			u.setId(SecUtils.getUUID());
 			u.setName("我是中国人，我会说中文"+i);
 			u.setInt1(u.getInt1()+i);
-			u.setInt2(u.getInt2()+i);
+			//u.setInt2(u.getInt2()+i);
 			list.add(u);
 		    //LuceneUtils.saveDocument(rootdir, u);
 		}
@@ -52,14 +52,14 @@ public class LuceneTest {
          u.setId(SecUtils.getUUID());
          u.setName("暴龙眼镜"+60);
          u.setInt1(u.getInt1()+60);
-         u.setInt2(u.getInt2()+60);
+         //u.setInt2(u.getInt2()+60);
          list.add(u);
          
          LuceneDto u2=new LuceneDto();
          u2.setId(SecUtils.getUUID());
          u2.setName("卫龙辣条"+61);
          u2.setInt1(u2.getInt1()+61);
-         u2.setInt2(u2.getInt2()+61);
+        // u2.setInt2(u2.getInt2()+61);
          list.add(u2);
          
          Long d2=System.currentTimeMillis();
@@ -185,6 +185,11 @@ public class LuceneTest {
            Long d3=System.currentTimeMillis();
            List<LuceneDto> list = LuceneUtils.searchDocument(rootdir,LuceneDto.class,null,lsc);
            System.out.println("查询"+list.size()+"耗时:"+(System.currentTimeMillis()-d3));
+           
+           for (LuceneDto u:list) {
+                System.out.println(u.getId()+","+u.getName()+","+u.getD1()+","+u.getF1()+","+u.getInt1()+","+u.getD2()+","+u.getF2()+","+u.getInt2()+","+u.getDate());
+                
+            }
            
            
        }
