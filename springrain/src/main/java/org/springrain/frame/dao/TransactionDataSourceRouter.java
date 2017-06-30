d17ec1f7470c55fb6e3beb13018422466e082fbe
caomei <caomei@DESKTOP-1T7L05U>
2017-06-30 21:21:52 +0800

    修改 数据库事务拦截

--- springrain/src/main/java/org/springrain/frame/dao/TransactionDataSourceRouter.java
+++ springrain/src/main/java/org/springrain/frame/dao/TransactionDataSourceRouter.java
@@ -1,8 +1,6 @@
 package org.springrain.frame.dao;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.NoTransactionException;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 /**
  * 配合spirng的事务拦截器,如果有事务就是写库,没有事务就是读库
@@ -13,14 +11,10 @@ public class TransactionDataSourceRouter extends AbstractRoutingDataSource {
 
 	@Override
 	protected Object determineCurrentLookupKey() {
-		
-		
-		try{
-            TransactionInterceptor.currentTransactionStatus();
-		}catch(NoTransactionException e){//没有事务,从库
-			return "dataSourceRead";
+		if(FrameDataSourceTransactionManager.isExistTransaction()) {
+			return "dataSourceWrite";
 		}
-		return "dataSourceWrite";
+		return "dataSourceRead";
 	}
 	
 
