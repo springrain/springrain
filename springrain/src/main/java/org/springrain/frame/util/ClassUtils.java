cbcc0e7af4e0219ba2ad3176986a05198acdd033
caomei <caomei@DESKTOP-1T7L05U>
2017-07-03 18:19:31 +0800

    默认值为NULL

--- springrain/src/main/java/org/springrain/frame/util/ClassUtils.java
+++ springrain/src/main/java/org/springrain/frame/util/ClassUtils.java
@@ -24,11 +24,19 @@ import javax.persistence.Transient;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.ConvertUtilsBean;
+import org.apache.commons.beanutils.ConvertUtilsBean2;
 import org.apache.commons.beanutils.converters.BigDecimalConverter;
+import org.apache.commons.beanutils.converters.BigIntegerConverter;
+import org.apache.commons.beanutils.converters.BooleanConverter;
+import org.apache.commons.beanutils.converters.ByteConverter;
+import org.apache.commons.beanutils.converters.CharacterConverter;
 import org.apache.commons.beanutils.converters.DoubleConverter;
+import org.apache.commons.beanutils.converters.FloatConverter;
 import org.apache.commons.beanutils.converters.IntegerConverter;
 import org.apache.commons.beanutils.converters.LongConverter;
 import org.apache.commons.beanutils.converters.ShortConverter;
+import org.apache.commons.beanutils.converters.StringConverter;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -64,11 +72,17 @@ public class ClassUtils {
 	 * 默认值为NULL
 	 */
 	static {
-		ConvertUtils.register(new LongConverter(null), Long.class);
 		ConvertUtils.register(new ShortConverter(null), Short.class);
 		ConvertUtils.register(new IntegerConverter(null), Integer.class);
+		ConvertUtils.register(new BigIntegerConverter(null),BigInteger.class);
+		ConvertUtils.register(new LongConverter(null), Long.class);
+		ConvertUtils.register(new FloatConverter(null), Float.class);
 		ConvertUtils.register(new DoubleConverter(null), Double.class);
 		ConvertUtils.register(new BigDecimalConverter(null), BigDecimal.class);
+		ConvertUtils.register(new BooleanConverter(null), Boolean.class);
+		ConvertUtils.register(new ByteConverter(null), Byte.class);
+		ConvertUtils.register(new CharacterConverter(null),Character.class);
+		ConvertUtils.register(new StringConverter(null),String.class);
 	}
 
 	
