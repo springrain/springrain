3ce1a6fcb0c54015523706c16da2432a6d3abd2e
caomei <caomei@DESKTOP-1T7L05U>
2017-07-19 11:02:28 +0800

    修改注释

--- springrain/src/main/java/org/springrain/system/dao/BaseSpringrainDaoImpl.java
+++ springrain/src/main/java/org/springrain/system/dao/BaseSpringrainDaoImpl.java
@@ -11,76 +11,73 @@ import org.springrain.frame.dao.dialect.IDialect;
 import org.springrain.frame.entity.AuditLog;
 
 /**
- *   springrain项目的基础Dao,代理demo数据库
+ * springrain项目的基础Dao,代理demo数据库
+ * 
  * @copyright {@link weicms.net}
  * @author springrain<Auto generate>
- * @version  2013-03-19 11:08:15
+ * @version 2013-03-19 11:08:15
  * @see org.springrain.system.dao.BaseSpringrainDaoImpl
  */
 @Repository("baseSpringrainDao")
-public class BaseSpringrainDaoImpl extends BaseJdbcDaoImpl implements IBaseJdbcDao{
+public class BaseSpringrainDaoImpl extends BaseJdbcDaoImpl implements IBaseJdbcDao {
+
+	public BaseSpringrainDaoImpl() {
+	}
 
 	/**
-	 * demo  数据库的jdbc,对应 spring配置的 jdbc bean
+	 * demo 数据库的jdbc,对应 spring配置的 jdbc bean
 	 */
 	@Resource
 	NamedParameterJdbcTemplate jdbc;
 	/**
-	 * demo  数据库的jdbcCall,对应 spring配置的 jdbcCall bean
+	 * demo 数据库的jdbcCall,对应 spring配置的 jdbcCall bean
 	 */
 	@Resource
 	public SimpleJdbcCall jdbcCall;
-    /**
-     * mysqlDialect 是mysql的方言,springBean的name,可以参考 IDialect的实现
-     */
+	/**
+	 * mysqlDialect 是mysql的方言,springBean的name,可以参考 IDialect的实现
+	 */
 	@Resource
 	public IDialect mysqlDialect;
+
 	@Override
 	public IDialect getDialect() {
 		return mysqlDialect;
 	}
 
-	public BaseSpringrainDaoImpl() {
-	}
-
-
 	/**
-	 * 实现父类方法,demo  数据库的jdbc,对应 spring配置的 jdbc bean
+	 * 实现父类方法,springrain数据库的jdbcCall,对应 spring配置的 jdbcCall bean
 	 */
 	@Override
 	public SimpleJdbcCall getJdbcCall() {
 		return this.jdbcCall;
 	}
+
 	/**
-	 * 实现父类方法,demo  数据库的jdbcCall,对应 spring配置的 jdbcCall bean
+	 * 实现父类方法,springrain数据库的jdbcCall,对应 spring配置的 jdbc bean
 	 */
 	@Override
 	public NamedParameterJdbcTemplate getJdbc() {
 		return jdbc;
 	}
-	
+
 	/*
 	 * 读写分离时,处理同步延迟问题
-	@Override
-	public NamedParameterJdbcTemplate getJdbc() {
-		try{
-            TransactionInterceptor.currentTransactionStatus();
-		}catch(NoTransactionException e){
-			return this.jdbc;
-		}
-		return getWriteJdbc();
-	}
-	*/
+	 * 
+	 * @Override public NamedParameterJdbcTemplate getJdbc() { try{
+	 * TransactionInterceptor.currentTransactionStatus();
+	 * }catch(NoTransactionException e){ return this.jdbc; } return getWriteJdbc();
+	 * }
+	 */
 
-
-/**
- * 实现父类方法,返回记录日志的Entity接口实现,reutrn null 则代表不记录日志
- */
+	/**
+	 * 实现父类方法,返回记录日志的Entity接口实现,reutrn null 则代表不记录日志
+	 */
 	@Override
 	public AuditLog getAuditLog() {
 		return null;
 	}
-	
+
 	/**
 	 * 是否打印sql语句,默认false
 	 * 
@@ -88,8 +85,7 @@ public class BaseSpringrainDaoImpl extends BaseJdbcDaoImpl implements IBaseJdbcD
 	 */
 	@Override
 	public boolean showsql() {
-		return true;
+		return false;
 	}
-
 
 }
