af3062c98bb749e24d8e4e4f10f6a3a051cdb2f7
caomei <caomei@DESKTOP-UR9TC61>
2018-04-21 20:19:48 +0800

    修改 代码位置,方便切换实现

--- springrain/src/main/java/org/springrain/config/ShiroConfig.java
+++ springrain/src/main/java/org/springrain/config/ShiroConfig.java
@@ -43,9 +43,7 @@ public class ShiroConfig {
     @Value("${springrain.session.timeout}")
     private Long sessionTimeout=1800000L;
     
-    //集群模式下,需要redis作为cache,注入redissonClient,单机模式需要注释掉
-   // @Resource
-  //  private RedissonClient redissonClient;
+
     
     
     
@@ -185,6 +183,11 @@ public class ShiroConfig {
      * @return
      */
     /*
+     
+      //集群模式下,需要redis作为cache,注入redissonClient,单机模式需要注释掉
+    @Resource
+    private RedissonClient redissonClient;
+     
     @Bean("shiroCacheManager")
     public CacheManager shiroCacheManager() {
         ShiroRedisCacheManager cacheManager=new ShiroRedisCacheManager();
