8c4a026503c3090d7b9eb74d16cc6938f82e5d57
caomei <caomei@DESKTOP-UR9TC61>
2018-07-25 16:40:03 +0800

    去掉默认请求第一页的业务逻辑

--- src/main/webapp/js/custom/common/springrain.js
+++ src/main/webapp/js/custom/common/springrain.js
@@ -296,8 +296,7 @@
 				}
 				var _action=jQuery("#"+formId).attr("action");
 				
-				// 分页重置为第一页
-				jQuery("#"+formId).attr("action",_that.appendToken(_action) + "&pageIndex=0").submit();
+				jQuery("#"+formId).attr("action",_that.appendToken(_action)).submit();
 				//jQuery("#"+formId).attr("action",_that.appendToken(_action)).submit();
 			},
 			/**
