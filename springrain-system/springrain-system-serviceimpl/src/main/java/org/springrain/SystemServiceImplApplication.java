25c39b4ee5a99b7a5e2625a4881cb965f50b8c1f
asony <619435955@qq.com>
2019-02-13 15:45:35 +0800

    单体运行需要注释掉

--- springrain-system/springrain-system-serviceimpl/src/main/java/org/springrain/SystemServiceImplApplication.java
+++ springrain-system/springrain-system-serviceimpl/src/main/java/org/springrain/SystemServiceImplApplication.java
@@ -1,7 +1,6 @@
 package org.springrain;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 作为单体运行时,需要注释 @SpringBootApplication 注解,会冲突,一个项目只能有一个@SpringBootApplication
@@ -10,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author caomei
  *
  */
-@SpringBootApplication
+//@SpringBootApplication
 public class SystemServiceImplApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SystemServiceImplApplication.class, args);
