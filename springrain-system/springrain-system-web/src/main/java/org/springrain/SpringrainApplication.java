1fc6b13b23422e16bc23575e33ef8935d73d9a27
springrain <chunanyong@163.com>
2019-09-19 15:51:58 +0800

    修改 pom 和配置

--- springrain-system/springrain-system-web/src/main/java/org/springrain/SpringrainApplication.java
+++ springrain-system/springrain-system-web/src/main/java/org/springrain/SpringrainApplication.java
@@ -1,7 +1,10 @@
 package org.springrain;
 
 import org.springframework.boot.SpringApplication;
-import org.springrain.frame.util.GlobalStatic;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 
 /**
  * 主入口,排除@Controller注解,主要为了Controller指定命名规则. 这个类所在的包,就是默认扫描的根包.
@@ -9,9 +12,9 @@ import org.springrain.frame.util.GlobalStatic;
  * @author caomei
  *
  */
-//@SpringBootApplication
-//@ComponentScan(basePackages = { "${springrain.basepackagepath}" }, excludeFilters = {
-//        @Filter(type = FilterType.ANNOTATION, value = Controller.class) })
+@SpringBootApplication
+@ComponentScan(basePackages = { "${springrain.basepackagepath}" },
+        excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Controller.class) })
 public class SpringrainApplication {
 
     public static void main(String[] args) {
