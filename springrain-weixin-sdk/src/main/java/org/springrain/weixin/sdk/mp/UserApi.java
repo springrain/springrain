64a1b8aff627825c7945d3f2af5d565149b35aca
springrain <chunanyong@163.com>
2019-11-22 12:59:43 +0800

    修改 注释

--- springrain-weixin-sdk/src/main/java/org/springrain/weixin/sdk/mp/UserApi.java
+++ springrain-weixin-sdk/src/main/java/org/springrain/weixin/sdk/mp/UserApi.java
@@ -1,8 +1,4 @@
-/**
- * Copyright (c) 2011-2014, James Zhan 詹波 (jfinal@126.com).
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
+
 
 package org.springrain.weixin.sdk.mp;
 
