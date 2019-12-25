41707ee554bd159f5ac0084740f3ca25f7e65d86
springrain <chunanyong@163.com>
2019-12-25 13:04:46 +0800

    seata sql 文件

--- springrain-system/springrain-system-web/sql/springrain.sql
+++ springrain-system/springrain-system-web/sql/springrain.sql
@@ -3,64 +3,78 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for branch_table
--- ----------------------------
-DROP TABLE IF EXISTS `branch_table`;
-CREATE TABLE `branch_table`  (
-  `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(128)  NOT NULL,
-  `transaction_id` bigint(20) NULL DEFAULT NULL,
-  `resource_group_id` varchar(32)  NULL DEFAULT NULL,
-  `resource_id` varchar(256)  NULL DEFAULT NULL,
-  `lock_key` varchar(128)  NULL DEFAULT NULL,
-  `branch_type` varchar(8)  NULL DEFAULT NULL,
-  `status` tinyint(4) NULL DEFAULT NULL,
-  `client_id` varchar(64)  NULL DEFAULT NULL,
-  `application_data` varchar(2000)  NULL DEFAULT NULL,
-  `gmt_create` datetime(0) NULL DEFAULT NULL,
-  `gmt_modified` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`branch_id`) ,
-  INDEX `idx_xid`(`xid`) 
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
 
--- ----------------------------
--- Table structure for global_table
--- ----------------------------
-DROP TABLE IF EXISTS `global_table`;
-CREATE TABLE `global_table`  (
-  `xid` varchar(128)  NOT NULL,
-  `transaction_id` bigint(20) NULL DEFAULT NULL,
-  `status` tinyint(4) NOT NULL,
-  `application_id` varchar(32)  NULL DEFAULT NULL,
-  `transaction_service_group` varchar(32)  NULL DEFAULT NULL,
-  `transaction_name` varchar(64)  NULL DEFAULT NULL,
-  `timeout` int(11) NULL DEFAULT NULL,
-  `begin_time` bigint(20) NULL DEFAULT NULL,
-  `application_data` varchar(2000)  NULL DEFAULT NULL,
-  `gmt_create` datetime(0) NULL DEFAULT NULL,
-  `gmt_modified` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`xid`) ,
-  INDEX `idx_gmt_modified_status`(`gmt_modified`, `status`) ,
-  INDEX `idx_transaction_id`(`transaction_id`) 
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+-- https://github.com/seata/seata/tree/develop/script
 
--- ----------------------------
--- Table structure for lock_table
--- ----------------------------
-DROP TABLE IF EXISTS `lock_table`;
-CREATE TABLE `lock_table`  (
-  `row_key` varchar(128)  NOT NULL,
-  `xid` varchar(96)  NULL DEFAULT NULL,
-  `transaction_id` mediumtext  NULL,
-  `branch_id` mediumtext  NULL,
-  `resource_id` varchar(256)  NULL DEFAULT NULL,
-  `table_name` varchar(32)  NULL DEFAULT NULL,
-  `pk` varchar(32)  NULL DEFAULT NULL,
-  `gmt_create` datetime(0) NULL DEFAULT NULL,
-  `gmt_modified` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`row_key`) 
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+-- for AT mode you must to init this sql for you business database. the seata server not need it.
+CREATE TABLE IF NOT EXISTS `undo_log`
+(
+    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT 'increment id',
+    `branch_id`     BIGINT(20)   NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(100) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME     NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME     NOT NULL COMMENT 'modify datetime',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4  COMMENT ='AT transaction mode undo table';
+
+
+-- -------------------------------- The script used when storeMode is 'db' --------------------------------
+-- the table to store GlobalSession data
+CREATE TABLE IF NOT EXISTS `global_table`
+(
+    `xid`                       VARCHAR(128) NOT NULL,
+    `transaction_id`            BIGINT,
+    `status`                    TINYINT      NOT NULL,
+    `application_id`            VARCHAR(32),
+    `transaction_service_group` VARCHAR(32),
+    `transaction_name`          VARCHAR(128),
+    `timeout`                   INT,
+    `begin_time`                BIGINT,
+    `application_data`          VARCHAR(2000),
+    `gmt_create`                DATETIME,
+    `gmt_modified`              DATETIME,
+    PRIMARY KEY (`xid`),
+    KEY `idx_gmt_modified_status` (`gmt_modified`, `status`),
+    KEY `idx_transaction_id` (`transaction_id`)
+)  ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+
+-- the table to store BranchSession data
+CREATE TABLE IF NOT EXISTS `branch_table`
+(
+    `branch_id`         BIGINT       NOT NULL,
+    `xid`               VARCHAR(128) NOT NULL,
+    `transaction_id`    BIGINT,
+    `resource_group_id` VARCHAR(32),
+    `resource_id`       VARCHAR(256),
+    `branch_type`       VARCHAR(8),
+    `status`            TINYINT,
+    `client_id`         VARCHAR(64),
+    `application_data`  VARCHAR(2000),
+    `gmt_create`        DATETIME,
+    `gmt_modified`      DATETIME,
+    PRIMARY KEY (`branch_id`),
+    KEY `idx_xid` (`xid`)
+)  ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+
+-- the table to store lock data
+CREATE TABLE IF NOT EXISTS `lock_table`
+(
+    `row_key`        VARCHAR(128) NOT NULL,
+    `xid`            VARCHAR(96),
+    `transaction_id` BIGINT,
+    `branch_id`      BIGINT       NOT NULL,
+    `resource_id`    VARCHAR(256),
+    `table_name`     VARCHAR(32),
+    `pk`             VARCHAR(36),
+    `gmt_create`     DATETIME,
+    `gmt_modified`   DATETIME,
+    PRIMARY KEY (`row_key`),
+    KEY `idx_branch_id` (`branch_id`)
+)  ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
 
 -- ----------------------------
 -- Table structure for t_article
@@ -696,23 +710,6 @@ CREATE TABLE `t_user_role`  (
 INSERT INTO `t_user_role` VALUES ('1', 'u_10001', 'r_10001', '2019-07-24 11:19:27', 'u_10001', '2019-07-24 11:19:27', 'u_10001', NULL, NULL, NULL, NULL, NULL);
 INSERT INTO `t_user_role` VALUES ('8a7f31289845414583f230839b98e98d', '23a2c0c52ed142938c159c9b9004fa35', 'e8a4ad9944894908b43ded631094dcbb', '2019-07-24 11:19:27', 'u_10001', '2019-07-24 11:19:27', 'u_10001', NULL, NULL, NULL, NULL, NULL);
 
--- ----------------------------
--- Table structure for undo_log
--- ----------------------------
-DROP TABLE IF EXISTS `undo_log`;
-CREATE TABLE `undo_log`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(100)  NOT NULL,
-  `context` varchar(128)  NULL DEFAULT NULL,
-  `rollback_info` longblob NOT NULL,
-  `log_status` int(11) NOT NULL,
-  `log_created` datetime(0) NOT NULL,
-  `log_modified` datetime(0) NOT NULL,
-  `ext` varchar(100)  NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `ux_undo_log`(`xid`, `branch_id`) 
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
 
 -- ----------------------------
 -- Table structure for wx_cpconfig
