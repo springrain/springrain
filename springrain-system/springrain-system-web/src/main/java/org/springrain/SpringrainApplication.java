bc03711e9c2f98acaaea7a0adceebebb34e58b9b
springrain <chunanyong@163.com>
2020-08-09 12:22:28 +0800

    修改注释

--- springrain-system/springrain-system-web/src/main/java/org/springrain/SpringrainApplication.java
+++ springrain-system/springrain-system-web/src/main/java/org/springrain/SpringrainApplication.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 
 /**
  * 主入口,排除@Controller注解,主要为了Controller指定命名规则. 这个类所在的包,就是默认扫描的根包.
+ * 注意:只能有一个@SpringBootApplication入口文件,如果其他模块引用了这个模块,就需要把类上的注解都注释掉
  *
  * @author caomei
  *
