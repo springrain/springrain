67c02f407d8efbd6fc9189ecfb6028091dd50259
springrain <chunanyong@163.com>
2020-08-10 14:19:29 +0800

    修改注释

--- springrain-frame-util/src/main/java/org/springrain/frame/util/JacksonDateDeserializer.java
+++ springrain-frame-util/src/main/java/org/springrain/frame/util/JacksonDateDeserializer.java
@@ -8,23 +8,27 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.util.Date;
 
+/**
+ * 全局处理jackson解析日期字符串,不需要在字段上加上日期格式的注解了.
+ * 自动分析字符串格式,转化成Date对象
+ */
 public class JacksonDateDeserializer extends DateDeserializers.DateDeserializer  {
     @Override
-    public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    public Date deserialize(JsonParser p, DeserializationContext context) throws IOException {
         if (p == null) {
-            return super.deserialize(p, ctxt);
+            return super.deserialize(p, context);
         }
         String text = p.getText();
         text = text.trim();
         if (StringUtils.isBlank(text)) {
-            return super.deserialize(p, ctxt);
+            return super.deserialize(p, context);
         }
         Date date=DateUtils.convertString2DateAutoFormat(text);
         if (date!=null){
             return date;
         }
 
-        return super.deserialize(p, ctxt);
+        return super.deserialize(p, context);
 
     }
 }
