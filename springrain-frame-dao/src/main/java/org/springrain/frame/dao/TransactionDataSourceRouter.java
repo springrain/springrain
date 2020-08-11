30854f6168032fc95a36f64e0c2318450a0014f3
springrain <chunanyong@163.com>
2020-08-11 14:18:03 +0800

    修改注释

--- springrain-frame-dao/src/main/java/org/springrain/frame/dao/TransactionDataSourceRouter.java
+++ springrain-frame-dao/src/main/java/org/springrain/frame/dao/TransactionDataSourceRouter.java
@@ -11,6 +11,8 @@ public class TransactionDataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
+        //不能判断当前是否有事务,因为事务是基于Connection的,如果有连接了,就意味着已经有dataSource了
+        //需要写个切面,设置一个ThreadLocal,在获取DataSource之前,把读写标识放到ThreadLocal,这里取值判断
         if (FrameDataSourceTransactionManager.isExistTransaction()) {
             return "dataSourceWrite";
         }
