bde55bbb59df807066b9a8cdfaec50eda5fd390b
springrain <chunanyong@163.com>
2020-08-16 20:42:03 +0800

    完事注释

--- springrain-frame-util/src/main/java/org/springrain/frame/util/DateUtils.java
+++ springrain-frame-util/src/main/java/org/springrain/frame/util/DateUtils.java
@@ -13,6 +13,9 @@ import java.util.Date;
  */
 public class DateUtils {
 
+    /**
+     * 默认时区
+     */
     public static final String DATE_TIMEZONE = "GMT+8";
 
     /**
@@ -27,10 +30,7 @@ public class DateUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(DateUtils.class);
 
-    /**
-     * 每天的毫秒数
-     */
-    // private final static long MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
+
     private DateUtils() {
         throw new IllegalAccessError("工具类不能实例化");
     }
@@ -59,32 +59,32 @@ public class DateUtils {
     /**
      * 转换字符串得到默认格式的日期类型
      *
-     * @param text
+     * @param dataStr 日期字符串
      * @return
      * @throws ParseException
      */
-    public static Date convertString2Date(String text) {
-        if (StringUtils.isBlank(text)){
+    public static Date convertString2Date(String dataStr) {
+        if (StringUtils.isBlank(dataStr)) {
             return null;
         }
         Date date = null;
         try {
-            if (text.length() <= 4) {
-                date = new SimpleDateFormat("yyyy").parse(text);
-            } else if (text.length() <= 7) {
-                date = new SimpleDateFormat("yyyy-MM").parse(text);
-            } else if (text.length() <= 10) {
-                date = new SimpleDateFormat("yyyy-MM-dd").parse(text);
-            } else if (text.length() <= 19 && text.contains("T")) {
-                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(text);
-            } else if (text.length() <= 19) {
-                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(text);
-            } else if (text.length() <= 23 && text.contains("T")) {
-                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss").parse(text);
-            } else if (text.length() <= 23) {
-                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.sss").parse(text);
-            } else if (text.length() <= 24) {//2020-08-09T06:57:20.078Z
-                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'").parse(text);
+            if (dataStr.length() <= 4) {
+                date = new SimpleDateFormat("yyyy").parse(dataStr);
+            } else if (dataStr.length() <= 7) {
+                date = new SimpleDateFormat("yyyy-MM").parse(dataStr);
+            } else if (dataStr.length() <= 10) {
+                date = new SimpleDateFormat("yyyy-MM-dd").parse(dataStr);
+            } else if (dataStr.length() <= 19 && dataStr.contains("T")) {
+                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(dataStr);
+            } else if (dataStr.length() <= 19) {
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dataStr);
+            } else if (dataStr.length() <= 23 && dataStr.contains("T")) {
+                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss").parse(dataStr);
+            } else if (dataStr.length() <= 23) {
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.sss").parse(dataStr);
+            } else if (dataStr.length() <= 24) {//2020-08-09T06:57:20.078Z
+                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'").parse(dataStr);
             }
         } catch (ParseException ex) {
             logger.error(ex.getMessage(), ex);
@@ -118,7 +118,7 @@ public class DateUtils {
     /**
      * 转换日期,得到默认日期格式字符串
      *
-     * @param targetDate
+     * @param targetDate 需要转化的日期
      * @return
      */
     public static String convertDate2String(Date targetDate) {
@@ -129,31 +129,35 @@ public class DateUtils {
     /**
      * 格式化日期
      *
+     * @param formatString 需要把目标日期格式化什么样子的格式。例如,yyyy-MM-dd HH:mm:ss
+     * @param targetDate   目标日期
      * @throws ParseException 例: DateUtils.formatDate("yyyy-MM-dd HH",new Date())
      *                        "yyyy-MM-dd HH:00:00"
      */
-    public static Date formatDate(String formatString, Date date) throws ParseException {
-        if (date == null) {
+    public static Date formatDate(String formatString, Date targetDate) throws ParseException {
+        if (targetDate == null) {
             return null;
         }
         if (StringUtils.isBlank(formatString)) {
             formatString = DateUtils.DATE_FORMAT;
         }
 
-        date = DateUtils.convertString2Date(formatString, DateUtils.convertDate2String(formatString, date));
+        targetDate = DateUtils.convertString2Date(formatString, DateUtils.convertDate2String(formatString, targetDate));
 
-        return date;
+        return targetDate;
     }
 
     /**
      * 格式化日期 yyyy-MM-dd
      *
+     * @param targetDate 目标日期
+     * @return
      * @throws ParseException 例： DateUtils.formatDate(new Date()) "yyyy-MM-dd
      *                        00:00:00"
      */
-    public static Date formatDate(Date date) throws ParseException {
-        date = formatDate(DateUtils.DATE_FORMAT, date);
-        return date;
+    public static Date formatDate(Date targetDate) throws ParseException {
+        targetDate = formatDate(DateUtils.DATE_FORMAT, targetDate);
+        return targetDate;
     }
 
 
