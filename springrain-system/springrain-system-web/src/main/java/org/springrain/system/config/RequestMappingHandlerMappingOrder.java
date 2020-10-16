30e8f2d8ffd1d308679e0292cd3d61b087ca8301
springrain <chunanyong@163.com>
2020-10-16 20:42:48 +0800

    风险:复写请求A和未复写的B存在逻辑关联关系,如果产品更新逻辑,复写的A和产品的B会存在逻辑兼容风险

--- springrain-system/springrain-system-web/src/main/java/org/springrain/system/config/RequestMappingHandlerMappingOrder.java
+++ springrain-system/springrain-system-web/src/main/java/org/springrain/system/config/RequestMappingHandlerMappingOrder.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 /**
  * 实现一个URI映射多个业务实现,通过@Order注解标识优先级,目前强制一个URI只能有一个@Order起效.
+ * 风险:复写请求A和未复写的B存在逻辑关联关系,如果产品更新逻辑,复写的A和产品的B会存在逻辑兼容风险
  */
 public class RequestMappingHandlerMappingOrder extends RequestMappingHandlerMapping {
 
@@ -30,11 +31,12 @@ public class RequestMappingHandlerMappingOrder extends RequestMappingHandlerMapp
         if (method.getDeclaringClass().isAnnotationPresent(Order.class)||method.isAnnotationPresent(Order.class)){//如果存在order,就删除掉已经注册的 普通mapping
             RequestMappingInfo oldMapping= requestMappingInfoMap.get(key);
             if (oldMapping!=null&&requestMappingInfoMap.get(orderKey)==null) {//如果没有order mapping映射,就删除这个普通映射
+                //删除掉映射
                 super.unregisterMapping(oldMapping);
             }
             //放入 order
             requestMappingInfoMap.put(orderKey,mapping);
-        }else if(requestMappingInfoMap.get(orderKey)!=null){//已经存在 order 排序的映射了,就不再处理 普通mapping了
+        }else if(requestMappingInfoMap.get(orderKey)!=null){//已经存在 order mapping 了,就不再处理 普通mapping了
             return;
         }else{//其他情况 作为 普通mapping
             requestMappingInfoMap.put(key,mapping);
