610d569e5294f550e4d0774e5c503484ed03c9ca
dingtalk_ownzfb <>
2021-04-13 11:33:54 +0800

    修改 泛型

--- springrain-frame-util/src/main/java/org/springrain/frame/util/ClassUtils.java
+++ springrain-frame-util/src/main/java/org/springrain/frame/util/ClassUtils.java
@@ -655,7 +655,7 @@ ClassUtils {
      */
     public static Class getActualTypeArgument(Class clazz){
         //获取子类所属接口的参数化类型,cn.xxx.xxx.BasicAction<cn.xxx.xxx.Standard>
-        Type type = clazz.getGenericInterfaces()[0];
+        Type type = clazz.getGenericSuperclass();
         //因为type是顶级接口没有定义任何方法，所以需要强转为子接口ParameterizedType
         ParameterizedType parameterizedType = (ParameterizedType) type;
 
