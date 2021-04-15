d1a2915459546ae7569282cb43a203637abeda24
dingtalk_ownzfb <>
2021-04-15 16:22:43 +0800

    修改注释

--- springrain-frame-cache-redis/src/main/java/org/springrain/frame/mq/IMessageProducerConsumerListener.java
+++ springrain-frame-cache-redis/src/main/java/org/springrain/frame/mq/IMessageProducerConsumerListener.java
@@ -23,10 +23,11 @@ public interface IMessageProducerConsumerListener<T> {
       /**
        * 消费消息,隔离Redis API,如果返回true则自动应答,如果返回false,认为消息处理失败.
        * 暂时注释这个方法,避免手动挡调用,这个方法是 StreamListener的onMessage主动调用的,正常不需要手动调用
+       * 还是要留着这个方法,用于增加 类似 @Transactional 事务注解扩展,因为是面向接口注入的
        * @param messageObjectDto
        * @return
        */
-      //boolean onMessage(MessageObjectDto<T> messageObjectDto);
+      boolean onMessage(MessageObjectDto<T> messageObjectDto);
 
       /**
        * 重试消息,项目启动时会重试一次,业务代码自行实现根据调度重试
