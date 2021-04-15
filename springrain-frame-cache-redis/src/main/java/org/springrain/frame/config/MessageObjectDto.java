5a5d16f592c18addae64fc04ac558433f896e56a
dingtalk_ownzfb <>
2021-04-15 15:15:51 +0800

    修改注释

--- springrain-frame-cache-redis/src/main/java/org/springrain/frame/config/MessageObjectDto.java
+++ springrain-frame-cache-redis/src/main/java/org/springrain/frame/config/MessageObjectDto.java
@@ -19,6 +19,13 @@ public class MessageObjectDto<T> {
      */
     private T messageObject;
 
+    /**
+     * 构造函数
+     * @param messageObject
+     * @param queueName
+     * @param messageId
+     * @param messageTime
+     */
     protected MessageObjectDto(T messageObject, String queueName, String messageId, Long messageTime){
         this.messageObject =messageObject;
         this.queueName=queueName;
@@ -27,23 +34,35 @@ public class MessageObjectDto<T> {
 
     }
 
-
+    /**
+     * 消息的ID
+     * @return
+     */
     public String getMessageId() {
         return messageId;
     }
 
 
+    /**
+     * 消息的时间戳
+     * @return
+     */
 
     public Long getMessageTime() {
         return messageTime;
     }
 
-
+    /**
+     * 队列名称
+     */
     public String getQueueName() {
         return queueName;
     }
 
-
+    /**
+     * 队列中的对象值
+     * @return
+     */
     public T getMessageObject() {
         return messageObject;
     }
