82b9d4043ba10ed5c9bb7543bd7bf6c6541af1f9
dingtalk_ownzfb <>
2021-04-16 18:12:16 +0800

    规范变量名

--- springrain-frame-cache-redis/src/main/java/org/springrain/frame/mq/BytesToTimestampConverter.java
+++ springrain-frame-cache-redis/src/main/java/org/springrain/frame/mq/BytesToTimestampConverter.java
@@ -13,8 +13,8 @@ import java.sql.Timestamp;
 //@ReadingConverter
 class BytesToTimestampConverter implements Converter<byte[], Timestamp> {
     @Override
-    public Timestamp convert(byte[] bytes) {
-        String value = new String(bytes);
+    public Timestamp convert(byte[] source) {
+        String value = new String(source);
         return new Timestamp(Long.parseLong(value));
     }
 
