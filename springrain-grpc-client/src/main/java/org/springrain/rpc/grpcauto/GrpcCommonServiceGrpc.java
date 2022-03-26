efb5b1be03f98caad820a4a6289116c626778c70
springrain <chunanyong@163.com>
2022-03-26 14:29:16 +0800

    升级pom

--- springrain-grpc-client/src/main/java/org/springrain/rpc/grpcauto/GrpcCommonServiceGrpc.java
+++ springrain-grpc-client/src/main/java/org/springrain/rpc/grpcauto/GrpcCommonServiceGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.43.2)",
+    value = "by gRPC proto compiler (version 1.45.0)",
     comments = "Source: grpcCommonService.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class GrpcCommonServiceGrpc {
