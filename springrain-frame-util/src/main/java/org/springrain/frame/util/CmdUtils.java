68781d31ab393fcba63c73b0678e8f34d6d7c2aa
springrain <chunanyong@163.com>
2024-09-28 14:18:07 +0800

    执行cmd设置超时时间

--- springrain-frame-util/src/main/java/org/springrain/frame/util/CmdUtils.java
+++ springrain-frame-util/src/main/java/org/springrain/frame/util/CmdUtils.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 执行命令行的工具类,需要提前设定执行权限
@@ -49,6 +50,7 @@ public class CmdUtils {
         try {
 
             Process p = pb.start();
+            p.waitFor(60, TimeUnit.SECONDS);
             br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line = null;
             List<String> list = new ArrayList<>();
